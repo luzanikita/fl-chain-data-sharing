5446b4830efa0bdd0f0ae9e9e84f0aad5104ef06
Nikita Luzan <luzanikita@gmail.com>
2024-05-28 23:57:18 +0300

    FL update. Auth + SSL added.

--- src/fl_train.py
+++ src/fl_train.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 from functools import partial
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
 import flwr as fl
 import numpy as np
@@ -28,8 +28,9 @@ class FlowerClient(fl.client.NumPyClient):
     def fit(
         self, parameters: List[np.ndarray], config: Dict[str, Scalar]
     ) -> Tuple[List[np.ndarray], int, Dict[str, Scalar]]:
+        local_epochs = config.get("local_epochs", 1)
         set_parameters(self.net, parameters)
-        train(self.net, self.trainloader, epochs=1)
+        train(self.net, self.trainloader, epochs=local_epochs)
         return get_parameters(self.net), len(self.trainloader), {}
 
     def evaluate(self, parameters: List[np.ndarray], config: Dict[str, Scalar]) -> Tuple[float, int, Dict[str, Scalar]]:
@@ -46,6 +47,19 @@ def set_parameters(net: nn.Module, parameters: List[np.ndarray]):
 
 def get_parameters(net: nn.Module) -> List[np.ndarray]:
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+
+def server_evaluate(
+    server_round: int,
+    parameters: fl.common.NDArrays,
+    config: Dict[str, fl.common.Scalar],
+    testloader: DataLoader,
+) -> Optional[Tuple[float, Dict[str, fl.common.Scalar]]]:
+    net = Net().to(DEVICE)
+    set_parameters(net, parameters)  # Update model with the latest parameters
+    loss, accuracy = test(net, testloader)
+    print(f"Server-side evaluation loss {loss} / accuracy {accuracy}")
+    return loss, {"accuracy": accuracy}
 
 
 def client_fn(
@@ -77,6 +91,19 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
     return {"accuracy": sum(accuracies) / sum(examples)}
 
 
+def fit_config(server_round: int):
+    """Return training configuration dict for each round.
+
+    Perform two rounds of training with one local epoch, increase to two local
+    epochs afterwards.
+    """
+    config = {
+        "server_round": server_round,  # The current round of federated learning
+        "local_epochs": 1 if server_round < 2 else 2,  #
+    }
+    return config
+
+
 if __name__ == "__main__":
     print(f"Training on {DEVICE} using PyTorch {torch.__version__}")
 
@@ -93,6 +120,11 @@ if __name__ == "__main__":
     # override the initial params
     init_params = get_parameters(Net())
 
+    trainloaders, valloaders, testloader = load_datasets()
+
+    parametrized_server_evaluate = partial(server_evaluate, testloader=testloader)
+    parametrized_client_fn = partial(client_fn, trainloaders=trainloaders, valloaders=valloaders)
+
     # Create FedAvg strategy
     strategy = fl.server.strategy.FedAvg(
         fraction_fit=1.0,
@@ -100,18 +132,16 @@ if __name__ == "__main__":
         min_fit_clients=NUM_CLIENTS,
         min_evaluate_clients=NUM_CLIENTS,
         min_available_clients=NUM_CLIENTS,
-        evaluate_metrics_aggregation_fn=weighted_average,
+        evaluate_metrics_aggregation_fn=weighted_average,  # distributed evaluation
+        evaluate_fn=parametrized_server_evaluate,  # centralized evaluation
         initial_parameters=fl.common.ndarrays_to_parameters(init_params),
+        on_fit_config_fn=fit_config,  # pass config values from server to clients
     )
-
-    trainloaders, valloaders, testloader = load_datasets()
-    parametrized_client_fn = partial(client_fn, trainloaders=trainloaders, valloaders=valloaders)
-
     # Start simulation
     fl.simulation.start_simulation(
         client_fn=parametrized_client_fn,
         num_clients=NUM_CLIENTS,
-        config=fl.server.ServerConfig(num_rounds=5),
+        config=fl.server.ServerConfig(num_rounds=3),
         strategy=strategy,
         client_resources=client_resources,
     )
